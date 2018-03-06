2e71a924dc1be2380ba4b1c8d9900251902d55f5
Sava <vuckovicsava11@gmail.com>
2018-03-06 09:18:27 +0100

    added my_inject, my_count, my_all? spec

--- spec/enumerable_extension_spec.rb
+++ spec/enumerable_extension_spec.rb
@@ -8,17 +8,16 @@ describe Enumerable do
 
   describe "#my_each" do
 
+    it "doesn't modify original array" do
+      @test_array.my_each { |x| x + 12 }
+
+      expect(@test_array).to eq([1,2,3,4])
+    end
+
     it "performs specified operation on all elements" do
       expected = [2,3,4,5]
       actual = []
       @test_array.my_each { |x| actual << x + 1 }
-
-      expect(actual).to eq(expected)
-    end
-
-    it "doesn't modify original array" do
-      expected = [1,2,3,4]
-      actual = @test_array.my_each { |x| x + 12 }
 
       expect(actual).to eq(expected)
     end
@@ -28,10 +27,9 @@ describe Enumerable do
   describe "#my_each_with_index" do
 
     it "doesn't modify original array" do
-      expected = [1,2,3,4]
-      actual = @test_array.my_each_with_index { |x, idx| x + 12 }
+      @test_array.my_each_with_index { |x, idx| x + 12 }
 
-      expect(actual).to eq(expected)
+      expect(@test_array).to eq([1,2,3,4])
     end
 
     it "performs each operation using index" do
@@ -46,9 +44,9 @@ describe Enumerable do
   describe "#my_select" do
 
     it "doesn't modify original array" do
-      actual = @test_array.my_select { |x| x + 12 }
+      @test_array.my_select { |x| x + 12 }
 
-      expect(actual).to eq(@test_array)
+      expect(@test_array).to eq([1,2,3,4])
     end
 
     it "returns correct elements based on predicate given" do
@@ -57,5 +55,80 @@ describe Enumerable do
 
       expect(actual).to eq(expected)
     end
+  end
+
+  describe "#my_inject" do
+
+    it "doesn't modify original array" do
+      @test_array.my_inject { |x, y| x + y }
+
+      expect(@test_array).to eq([1,2,3,4])
+    end
+
+    context "given a block { |x, y| x + y }" do
+      it 'returns 10' do
+        actual = @test_array.my_inject { |x, y| x + y }
+
+        expect(actual).to eq(10)
+      end
+    end
+
+    context "given a block { |x, y| x * y }" do
+      it 'returns 24' do
+        actual = @test_array.my_inject { |x, y| x * y }
+
+        expect(actual).to eq(24)
+      end
+    end
+  end
+
+  describe "#my_count" do
+
+    it "doesn't modify original array" do
+      @test_array.my_count { |x| x }
+
+      expect(@test_array).to eq([1,2,3,4])
+    end
+
+    context "given a block { |x| x > 2 }" do
+      it 'returns 2' do
+        actual = @test_array.my_count { |x| x > 2 }
+
+        expect(actual).to eq(2)
+      end
+    end
+
+    context "given a block { |x| x.between?(1,3) }" do
+      it 'returns 3' do
+        actual = @test_array.my_count { |x| x.between?(1,3) }
+
+        expect(actual).to eq(3)
+      end
+    end
+  end
+
+  describe "#my_all?" do
+
+    it "doesn't modify original array" do
+      @test_array.my_all? { |x| x }
+
+      expect(@test_array).to eq([1,2,3,4])
+    end
+
+    context "given a block { |x| x > 2 }" do
+      it 'returns false' do
+        actual = @test_array.my_all? { |x| x > 2 }
+
+        expect(actual).to eq(false)
+      end
+    end
+
+    # context "given a block { |x, y| x.between?(1,3) }" do
+    #   it 'returns 3' do
+    #     actual = @test_array.my_count { |x, y| x.between?(1,3) }
+
+    #     expect(actual).to eq(3)
+    #   end
+    # end
   end
 end
